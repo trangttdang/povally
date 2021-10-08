c89646d847557eff809cc1c0d8130e0745ef6615
trangttdang <84547454+trangttdang@users.noreply.github.com>
2021-10-08 01:21:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// function initMap(data) {
-//     var flag = {lat: XXX, lng: YYY};
-//     var map = new google.maps.Map( document.getElementById('map'), {zoom: 4, center: flag});
-//     var marker = new google.maps.Marker({position: flag, map: map}); } src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBxqMOLD18Q38ZEqTI_Wi-0VctZ1m41H7Q&callback=initMap">
-// }
+
 
 let map;
 
